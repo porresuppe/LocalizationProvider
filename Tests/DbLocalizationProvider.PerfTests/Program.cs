bc7068e5c0a9012cdc3e322c1a8fe7aa1f54eabd
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-11-08 22:50:40 +0000

    added benchmark perf test project

---
+++ Tests/DbLocalizationProvider.PerfTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using DbLocalizationProvider.Internal;
+
+namespace DbLocalizationProvider.PerfTests
+{
+    [MemoryDiagnoser]
+    public class ExpressionHelperTests
+    {
+        [Benchmark]
+        public string GetTranslation() => ExpressionHelper.GetFullMemberName(() => SomeResource.Property1);
+    }
+
+    public class SomeResource
+    {
+        public static string Property1 { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ExpressionHelperTests>();
+        }
+    }
+}
